22a190e860c7abd798c72d248c677bc7d63b6cde
JuanCarlos Lopez <jucarlo86@gmail.com>
2026-08-04 14:05:32 -0600

    34 publish reply correlation-id

--- src/main/java/dev/jclp/demo/product_command/handler/ProductCommandConsumer.java
+++ src/main/java/dev/jclp/demo/product_command/handler/ProductCommandConsumer.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -24,22 +26,28 @@ public class ProductCommandConsumer {
     }
 
     @Bean
-    public Function<Command<ProductDto>, Reply<?>> handlerCommands() {
+    public Function<Message<Command<ProductDto>>, Message<Reply<?>>> handlerCommands() {
 
-        return command -> {
+
+
+        return msg -> {
+
+            Command<ProductDto> command = msg.getPayload();
             String type = command.type() == null ? "" : command.type().toUpperCase();
+
+            Reply<ProductDto> reply;
 
             switch (type) {
                 case "CREATE":
                     LOGGER.info("Received CREATE command");
                     if (command.body() == null) {
                         LOGGER.warn("Received CREATE command with null body");
-                        return new Reply<>("ERROR", "Command body cannot be null for CREATE operation", null);
+                        reply = new Reply<>("ERROR", "Command body cannot be null for CREATE operation", null);
                     }
                     LOGGER.info("Received CREATE command for product: {}", command.body());
                     ProductDto createdProduct = productService.create(command.body());
                     LOGGER.info("Product created successfully: {}", createdProduct);
-                    return new Reply<>("SUCCESS", "Product created successfully", createdProduct);
+                    reply = new Reply<>("SUCCESS", "Product created successfully", createdProduct);
 //                case "UPDATE":
 //                    LOGGER.info("Received UPDATE command");
 //                    // Handle update command
@@ -52,8 +60,17 @@ public class ProductCommandConsumer {
 //                    break;
                 default:
                     LOGGER.error("Unhandled command type: {}", type);
-                    return new Reply<>("ERROR", "Unhandled command type: " + type, null);
+                    reply = new Reply<>("ERROR", "Unhandled command type: " + type, null);
             }
+
+            String correlationId = msg.getHeaders().get("correlationId", String.class);
+            LOGGER.info("Receiving Correlation ID: {}", correlationId);
+
+            MessageBuilder<Reply<?>> out = MessageBuilder.withPayload(reply);
+            if (correlationId != null) {
+                out.setHeader("correlationId", correlationId);
+            }
+            return out.build();
         };
     }
 }
